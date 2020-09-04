342700c4c38f2a843b1b03d185ab855dd6c9e49e
Caio Landau <caioflandau@Caios-MacBook-Pro.local>
2020-09-04 19:31:36 -0300

    Tests for API layer

--- app/src/main/java/com/caiolandau/devigetredditclient/domain/api/Api.kt
+++ app/src/main/java/com/caiolandau/devigetredditclient/domain/api/Api.kt
@@ -1,9 +1,17 @@
 package com.caiolandau.devigetredditclient.domain.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class Api(
-    retrofitFactory: RetrofitFactory = RetrofitFactory()
+    moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build(),
+    redditBaseUrl: String = RedditApi.baseUrl
 ) {
-    val reddit: RedditApi = retrofitFactory
-        .build(RedditApi.baseUrl)
+    val reddit: RedditApi = Retrofit.Builder()
+        .baseUrl(redditBaseUrl)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
         .create(RedditApi::class.java)
 }
---
+++ app/src/test/java/com/caiolandau/devigetredditclient/domain/api/ApiUnitTests.kt
@@ -0,0 +1,74 @@
+package com.caiolandau.devigetredditclient.domain.api
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import retrofit2.HttpException
+import java.io.BufferedReader
+
+@Suppress("BlockingMethodInNonBlockingContext") // This is fine in a unit test
+class ApiUnitTests {
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer()
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun test_redditApi_getTopPostsTodayPage_success() = runBlockingTest {
+        mockWebServer.enqueue(MockResponse().setBody(readFileToString("reddit_posts_response.json")))
+        mockWebServer.start()
+
+        val baseUrl = mockWebServer.url("/top/")
+        val subject = makeSubject(baseUrl.toString()).reddit
+
+        val response = runBlocking {
+            subject.getTopPostsTodayPage(30, "123_abc", null)
+        }
+
+        // Compares that the data matches what the response JSON has:
+        assertEquals("mocked_after", response.data.after)
+        assertEquals(3, response.data.children.size)
+        assertEquals("First post's title", response.data.children.first().data.title)
+    }
+
+    @Test
+    fun test_redditApi_getTopPostsTodayPage_failure() = runBlockingTest {
+        // Mock an error response:
+        mockWebServer.enqueue(MockResponse().setResponseCode(500))
+        mockWebServer.start()
+
+        val baseUrl = mockWebServer.url("/top/")
+        val subject = makeSubject(baseUrl.toString()).reddit
+
+        try {
+            runBlocking {
+                subject.getTopPostsTodayPage(30, "123_abc", null)
+            }
+            // If the above didn't throw, the test failed (it should generate an error)
+            fail("Request didn't throw!")
+        } catch (ex: HttpException) {
+            assertEquals(500, ex.code())
+        }
+    }
+
+    private fun makeSubject(redditBaseUrl: String) = Api(redditBaseUrl = redditBaseUrl)
+
+    private fun readFileToString(filename: String): String {
+        val stream = javaClass.classLoader?.getResourceAsStream("reddit_posts_response.json")
+            ?: throw Exception("Could not read JSON from $filename")
+
+        return stream.bufferedReader().use(BufferedReader::readText)
+    }
+}
